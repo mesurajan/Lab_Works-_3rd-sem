b6ba0d96ccc91308d757074bbabccd0165d6e9b9
Surajan shrestha <surajansth1995@gmail.com>
2024-02-28 20:20:23 +0545

    added practice folders

--- DSA/lab_02/circular-practice.c
+++ DSA/lab_02/circular-practice.c
@@ -1,84 +1,88 @@
-// Implementation of circular queue
 #include <stdio.h>
 #include <conio.h>
-#define max 5
+#include <stdlib.h>
+#define n 5
 int main()
 {
-    int c, rear = -1, front = -1, new_data, myqueue[max], temp, front_temp, rear_temp, i;
+    int c, rear = -1, front = -1, new_data, temp, max, front_temp, rear_temp, i, myqueue[n];
     do
     {
-        printf("\nMENU\n1. Enqueue\n2. Dequeue\n3. Display\n4. Exit:\n Enter the task you want to perform\n");
+        printf("\nMenu\n1.Enqueue\n2.Dequeue\n3.Display\n4.Exit\nEnter your choice:\n");
         scanf("%d", &c);
+
         switch (c)
         {
+
         case 1:
             if ((front == 0 && rear == max - 1) || (front == (rear + 1) % max))
             {
-                printf("Queue Overflow........\n");
-                break;
+
+                printf("overflow\n");
             }
             else if (front == -1 && rear == -1)
             {
+                printf("enter the data you wanat to enqueue:\n");
+                scanf("%d", new_data);
                 front = 0;
                 rear = 0;
-                printf("Enter the number you want to enqueue:\n");
-                scanf("%d", &new_data);
                 myqueue[rear] = new_data;
             }
             else
             {
+                printf("Enter ten you want to enqueue:\n");
+                scanf("%d", new_data);
                 rear = (rear + 1) % max;
-                printf("Enter the number you want to enqueue:\n");
-                scanf("%d", &new_data);
                 myqueue[rear] = new_data;
             }
             break;
+
         case 2:
             if (front == -1 && rear == -1)
             {
-                printf("Queue Underflow..........\n");
-                break;
+                printf("underflow\n");
             }
             else if (front == rear)
             {
                 temp = myqueue[front];
                 front = -1;
                 rear = -1;
-                printf("The deleted data is %d\n", temp);
+                printf("deleted element is:%d", temp);
             }
             else
             {
                 temp = myqueue[front];
                 front = (front + 1) % max;
-                printf("The deleted data is %d\n", temp);
+                printf("Deleted element is :%d", temp);
             }
             break;
+
         case 3:
             front_temp = front;
             rear_temp = rear;
             if (front_temp != -1)
+
             {
                 if (front_temp <= rear_temp)
                 {
-                    printf("Item at Circular Queue\n");
+                    printf("Elements of circular queue\n");
                     for (i = front_temp; i <= rear_temp; i++)
                     {
-                        printf("%d\t", myqueue[i]);
+                        printf("%d", myqueue[i]);
                     }
                 }
                 else
                 {
                     for (i = front_temp; i < max; i++)
                     {
-                        printf("%d\t", myqueue[i]);
+                        printf("%d", myqueue[i]);
                     }
                     for (i = 0; i <= rear_temp; i++)
                     {
-                        printf("%d\t", myqueue[i]);
+                        printf("%d", myqueue[i]);
                     }
                 }
             }
-            printf("Queue Underflow............\n");
+            printf("Underflow\n");
         }
     } while (c < 4);
     return 0;
--- DSA/lab_02/linearqueue.c
+++ DSA/lab_02/linearqueue.c
@@ -1,74 +1,79 @@
-// Implementation of queue
 #include <stdio.h>
-#include <stdlib.h>
 #define n 5
 int main()
 {
-    int c, rear = -1, myqueue[n], new_data, temp, i, front = -1;
+    int c, rear = -1, front = -1, new_data, temp, i, myqueue[n];
     do
     {
-        printf("Enter your choice\n1.Enque\n2.Dequeue\n3.Display\n4.Exit\n");
-        printf("------------------------\n");
-        printf("Enter your choice:");
+        printf("\nMenu\n1.Enque\n2.DEque\n3.Display\n4.Exit\nEnter your choice\n");
         scanf("%d", &c);
+
         switch (c)
         {
         case 1:
             if (rear == n - 2)
             {
-                printf("\nQueue overflow\n");
+                printf("queue overflow");
             }
+
             else if (rear == -1)
             {
-                printf("\n Enter the new data to be Pushed\n");
-                scanf("%d", &new_data);
+                printf("Enter the element you want to push  in queue:\n");
+                scanf("%d", new_data);
                 front = 0;
                 rear = 0;
                 myqueue[rear] = new_data;
             }
             else
             {
-                printf("\n Enter new data to be pushed\n");
-                scanf("%d", &new_data);
+                printf("Enter the datat to be pushed ");
+                scanf("%d", new_data);
                 rear++;
                 myqueue[rear] = new_data;
             }
             break;
+
         case 2:
+
             if (front == -1)
             {
-                printf("\n underflow");
+                printf("queue underflow");
             }
             else if (front == rear)
-
             {
                 temp = myqueue[front];
-                printf("\n Deleted Element=%d\n", temp);
                 front = -1;
                 rear = -1;
+                printf("deleted element is:%d ", temp);
             }
             else
             {
                 temp = myqueue[front];
                 front++;
-                printf("\n Deleted Elements=%d\n", temp);
+                printf("Deleted element is :%d", temp);
             }
             break;
+
         case 3:
+
             if (rear == -1)
             {
-                printf("\n Queue underflow\n");
+
+                printf("queue underflow");
             }
             else
             {
+
                 for (i = front; i <= rear; i++)
                 {
+
                     printf("%d\t", myqueue[i]);
                 }
             }
         default:
-            printf("\n Enter the value again");
+            printf("enter the value again");
         }
     } while (c < 4);
+
     return 0;
 }
